8c9fcd5f80d5963327810f4689a35729afdc2d69
Daniel Patru <daniel.patru@samraksh.com>
2014-12-31 17:35:44 -0500

    Add avgs.

--- runningAverage.h
+++ runningAverage.h
@@ -1,15 +1,22 @@
 /// running average
+#include <vector>
 
 struct RunningAverage {
   double *nums;
+  double *_avgs;
+  /* vector<double> nums; */
+  /* vector<double> _avgs; */
   unsigned size; // maximum size of the running average
   unsigned currentIndex; // always points where a new value will go
   bool isFull; // true if at least size values have been submitted
+
   RunningAverage ( unsigned size ) : size(size), currentIndex(0),
-    isFull(false), nums( new double[size] ){};
+    isFull(false), nums( new double[size] ), _avgs( new double[size] ){};
+
   double operator() ( double x ) {
     // add x to nums and increment the current index
     this->nums [ this->currentIndex ] = x;
+    unsigned oldIndex = this->currentIndex;
     this->currentIndex = (this->currentIndex + 1 ) % this->size;
     if ( this->currentIndex == 0 ) isFull = true;
     
@@ -19,6 +26,18 @@ struct RunningAverage {
     while ( i < n ) sum += this->nums[i++];
     return sum / n;
   };
+
+  double *avgs() {
+    unsigned n = isFull? size: currentIndex;
+    double sum = 0;
+    for ( unsigned i = 0; i < n; i++ ) {
+      double num = nums [ ( currentIndex + n - 1 - i ) % n ];
+      sum += num;
+      _avgs [ i ] = i > 0? sum / (1+i): sum;
+    }
+    return _avgs;
+  };
+  
   double latest() {
     return this->nums[ this->currentIndex ];
   };
--- test.cpp
+++ test.cpp
@@ -3,9 +3,15 @@
 #include <stdlib.h>
 
 int main ( int argc, char *argv[] ) {
+  unsigned n = 3;
   RunningAverage avg(3); 
   for ( int i = 0; i < argc; i++ ) {
     double x = atof(argv[i]);
-    printf ( "adding %lf: running average is now %lf\n", x, avg(x) );
+    printf ( "\nadding %lf: running average is now %lf\n", x, avg(x) );
+    double *avgs = avg.avgs();
+    unsigned n = avg.isFull? avg.size: avg.currentIndex;
+    for ( int i = 0; i < n; i++ ) {
+      printf ( "  %d: %lf\n", i, avgs[ i ] );
+    }
   }
 };
