264cebe66c4d9d21325fc717c6814515bd6d7f34
Daniel Patru <daniel.patru@samraksh.com>
2015-01-03 20:39:11 -0500

    Add test for runningRatio.

--- test.cpp
+++ test.cpp
@@ -5,13 +5,16 @@
 int main ( int argc, char *argv[] ) {
   unsigned n = 3;
   RunningAverage avg(3); 
+  RunningRatio rat1(3), rat2(3); 
   for ( int i = 0; i < argc; i++ ) {
     double x = atof(argv[i]);
-    printf ( "\nadding %lf: running average is now %lf\n", x, avg(x) );
+    printf ( "\nadding %lf: avg=%lf, rat1=%lf, rat2=%lf\n", x, avg(x), rat1(x, 1), rat2 (x, 2) );
     vector<double> &avgs = avg.avgs();
+    vector<double> &rats1 = rat1.avgs();
+    vector<double> &rats2 = rat2.avgs();
     unsigned n = avgs.size();
     for ( int i = 0; i < n; i++ ) {
-      printf ( "  %d: %lf\n", i, avgs[ i ] );
+      printf ( "  %d: avg %lf, rat1 %lf, rat2 %lf\n", i, avgs[ i ], rats1 [ i ], rats2 [ i ] );
     }
   }
 };
